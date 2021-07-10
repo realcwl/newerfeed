4d80a6bc8a4e6be972cf9f2a8ce4fc67c7c252c3
Warren <warren@warrens-MacBook-Pro.local>
2021-07-10 14:59:56 -0700

    Remove landing directory

--- landing/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-const base = require('../.eslintrc')
-
-module.exports = {
-  ...base,
-  rules: {
-    ...base.rules,
-    'react/react-in-jsx-scope': 'off',
-  },
-}
--- landing/src/hooks/use-force-rerender.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react'
-
-export function useForceRerender() {
-  const [, setValue] = useState(0)
-
-  return () => setValue((value) => value + 1)
-}
