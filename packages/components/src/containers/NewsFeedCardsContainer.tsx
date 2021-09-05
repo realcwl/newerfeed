1488e4eb01cf1781e8ca7050ec12c6f927fdd460
Luismorlan <chenweilunster@gmail.com>
2021-09-05 11:06:41 -0700

    remove console.log

--- packages/components/src/containers/NewsFeedCardsContainer.tsx
+++ packages/components/src/containers/NewsFeedCardsContainer.tsx
@@ -58,7 +58,6 @@ export const NewsFeedCardsContainer = React.memo(
         columnId={columnId}
         errorMessage={''}
         fetchNextPage={() => {
-          console.log('load more')
           dispatch(
             actions.fetchColumnDataRequest({
               columnId,
