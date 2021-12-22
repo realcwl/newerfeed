90827d0619d3a7336bd829aa1977b59dcea0aa67
homer <sdi.gbn@gmail.com>
2021-12-21 22:13:40 -0800

    Revert "Revert "Merge pull request #157 from Luismorlan/sync-post-read-status" (#166)"
    This reverts commit b3a74b91f21407ebe9b749267df826061213b970.

--- packages/components/src/components/cards/BaseCard.tsx
+++ packages/components/src/components/cards/BaseCard.tsx
@@ -37,9 +37,9 @@ import FileDownloader from '../../libs/file-downloader'
 import { useHistory } from '../../libs/react-router'
 import {
   capatureView,
-  markItemAsRead,
-  markItemAsSaved,
-  markItemDuplicationAsRead,
+  setItemSavedStatus,
+  setItemDuplicationReadStatus,
+  setItemsReadStatus,
 } from '../../redux/actions'
 import { Link } from '../common/Link'
 import { useFastScreenshot } from '../../hooks/use-fast-screenshot'
@@ -403,12 +403,17 @@ export const BaseCard = React.memo((props: BaseCardProps) => {
             // 1. user already read all duplication messages
             // 2. user must also read the original message
             dispatch(
-              markItemDuplicationAsRead({ itemNodeId: nodeIdOrId, read: true }),
+              setItemDuplicationReadStatus({
+                itemNodeId: nodeIdOrId,
+                read: true,
+                syncup: true,
+              }),
             )
             dispatch(
-              markItemAsRead({
+              setItemsReadStatus({
                 itemNodeIds: [nodeIdOrId],
                 read: true,
+                syncup: true,
               }),
             )
           }}
@@ -659,7 +664,7 @@ export const BaseCard = React.memo((props: BaseCardProps) => {
                       style={styles.actionIcon}
                       onPress={() => {
                         dispatch(
-                          markItemAsSaved({
+                          setItemSavedStatus({
                             itemNodeId: nodeIdOrId,
                             save: !isSaved,
                           }),
@@ -724,9 +729,10 @@ export const BaseCard = React.memo((props: BaseCardProps) => {
                           color="foregroundColorMuted65"
                           onPress={() =>
                             dispatch(
-                              markItemAsRead({
+                              setItemsReadStatus({
                                 itemNodeIds: [nodeIdOrId],
                                 read: true,
+                                syncup: true,
                               }),
                             )
                           }
