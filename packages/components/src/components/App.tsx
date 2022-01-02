84999a6c3c1a38fa5534925af830105973932806
Yifan Zhou <evanfan.zhou@gmail.com>
2022-01-01 21:44:08 -0800

    NEWS-350 client side OneSignal SDK integration
    https://documentation.onesignal.com/docs/react-native-sdk-setup

--- packages/components/src/components/App.tsx
+++ packages/components/src/components/App.tsx
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react'
 import '../libs/analytics'
 
 import { ErrorBoundary } from '../libs/bugsnag'
+import { oneSignalInit } from '../libs/oneSignal'
 import { AppNavigator } from '../navigation/AppNavigator'
 import { enableNetworkInterceptors } from '../network-interceptor'
 import { AppGlobalStyles } from './AppGlobalStyles'
@@ -11,10 +12,7 @@ import { AppIconBadge } from './common/AppIconBadge'
 import { UnreadCountProvider } from './context/UnreadCountContext'
 
 enableNetworkInterceptors()
-
-/**
- *
- */
+oneSignalInit()
 
 // TODO: Enable StrictMode after react-native fixes it
 // @see https://github.com/facebook/react-native/issues/22186
@@ -27,7 +25,6 @@ export function App() {
         <AppProviders>
           <AppGlobalStyles key="app-global-styles" />
           <AppNavigator key="app-navigator" />
-
           <UnreadCountProvider>
             <AppIconBadge />
           </UnreadCountProvider>
